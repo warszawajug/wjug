9030b577bce74adf8c1399afa6a550b4e0da0837
Jakub Nabrdalik <jakubn@gmail.com>
2013-05-29 10:50:27 +0200

    updates to deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,15 @@ verifyAnyChangesAreToBeCommitted() {
     gitCommitStatus=$(git status --porcelain)
     if [ "$gitCommitStatus" == "" ]; then
         startPrintingRed
-        echo "Nothing new in here. Returning to master without a commit (including hard reset)."
+            echo "Nothing new in here. Returning to master without a commit (including hard reset)."
         stopColloringEcho
         cleanAndReturnToMaster
         exit 1
     fi
 
-    echo "Found changes to be committed."
+    startPrintingYellow
+        echo "Found changes to be committed."
+    stopColloringEcho
 }
 
 cleanAndReturnToMaster() {
@@ -25,10 +27,10 @@ verifyEverythingIsCommited() {
     gitCommitStatus=$(git status --porcelain)
     if [ "$gitCommitStatus" != "" ]; then
         startPrintingRed
-        echo "You have uncommited files."
-        echo "Your git status:"
-        echo $gitCommitStatus
-        echo "Sorry. Rules are rules. Aborting!"
+            echo "You have uncommited files."
+            echo "Your git status:"
+            echo $gitCommitStatus
+            echo "Sorry. Rules are rules. Aborting!"
         stopColloringEcho
         exit 1
     fi
@@ -38,10 +40,10 @@ verifyEverythingIsPushedToOrigin() {
     gitPushStatus=$(git cherry -v)
     if [ "$gitPushStatus" != "" ]; then
         startPrintingRed
-        echo "You have local commits that were NOT pushed."
-        echo "Your 'git cherry -v' status:"
-        echo $gitPushStatus
-        echo "Sorry. Rules are rules. Aborting!"
+            echo "You have local commits that were NOT pushed."
+            echo "Your 'git cherry -v' status:"
+            echo $gitPushStatus
+            echo "Sorry. Rules are rules. Aborting!"
         stopColloringEcho
         exit 1
     fi
@@ -49,13 +51,16 @@ verifyEverythingIsPushedToOrigin() {
 
 startPrintingYellow() {
     echo -e "\e[01;33m"
+    echo "================================================================================"
 }
 
 startPrintingRed() {
-    echo -e "\e[01;31m"
+    echo -e "\e[01;3m"
+    echo "================================================================================"
 }
 
 stopColloringEcho() {
+    echo "================================================================================"
     echo -n -e '\e[00m'
 }
 
@@ -70,14 +75,12 @@ stopColloringEcho() {
 # pull from master
 git checkout master
 git pull origin master
-currentHasOnMaster=$(git rev-parse HEAD)
+currentHashOnMaster=$(git rev-parse HEAD)
 
 # fire up nodejs and generate _public
 startPrintingYellow
-echo "================================================================================"
-echo "> Firing up nodejs. Ctrl+c when generations is finished."
-echo "> (a pull request to automatize it would be welcome)"
-echo "================================================================================"
+    echo "> Firing up nodejs. Ctrl+c when generations is finished."
+    echo "> (a pull request to automatize it would be welcome)"
 stopColloringEcho
 
 ./scripts/server.sh
@@ -94,15 +97,13 @@ verifyAnyChangesAreToBeCommitted
 # add & commit & push
 
 git add .
-echo 'autoupdate to $currentHasOnMaster '
-#git commit -m 'autoupdate to $currentHasOnMaster '
-#git push origin gh-pages
+echo "'autoupdate to $currentHashOnMaster '"
+git commit -m "'autoupdate to $currentHashOnMaster '"
+git push origin gh-pages
 
 # checkout master
-##cleanAndReturnToMaster
+cleanAndReturnToMaster
 
 startPrintingYellow
-echo "================================================================================"
-echo "Update finished. Thank you for cooperation."
-echo "================================================================================"
+    echo "Update finished. Thank you for cooperation."
 stopColloringEcho
