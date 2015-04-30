0093edfe0d0271ea6bb439ad1c533016da4005ff
Marcin Jasion <marcin.jasion@gmail.com>
2015-04-30 10:00:54 +0200

    autoupdate to ffbd5d795611a0a2add1e0a45aea8237fbfd1a68

--- js/app.js
+++ js/app.js
@@ -22,14 +22,14 @@ App.config([
     }).when('/by-you', {
       action: 'by-you'
     }).when('/newsletter', {
-      action: 'newsletter6'
+      action: 'newsletter7'
     });
     for (num = _i = 1; _i <= 200; num = ++_i) {
       $routeProvider.when("/meeting/" + num, {
         action: "meeting" + num
       });
     }
-    for (num = _j = 1; _j <= 6; num = ++_j) {
+    for (num = _j = 1; _j <= 7; num = ++_j) {
       $routeProvider.when("/newsletter/" + num, {
         action: "newsletter" + num
       });
