1df7e5069ae8b482ae450c981243b11050214a67
Michał Niczyporuk <m.niczyporuk@polish-airports.com>
2017-01-25 13:10:20 +0100

    improve server script

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 rm -rf _public
+rm app/assets/index.html
+rm app/assets/partials/*.html
+rm app/assets/partials/meetings/*.html
+rm app/assets/partials/newsletter/*.html
 node_modules/.bin/brunch watch --server
